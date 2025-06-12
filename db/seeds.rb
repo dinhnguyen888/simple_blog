0ea58ab9d7996a01becc977579d19c46ffed96c2
Nguyen Phuc Dinh <2251120286@ut.edu.vn>
2025-06-12 21:11:15 +0700

    style: add some effect for delete button in edit page

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ user.update!(
   role: "admin"
 )
 
-100.times do |i|
-  blog_post = BlogPost.where(title: "Blog Post #{i}").first_or_initialize
-  blog_post.update!(body: "Hello world", published_at: Time.current)
-end
+# 100.times do |i|
+#   blog_post = BlogPost.where(title: "Blog Post #{i}").first_or_initialize
+#   blog_post.update!(body: "Hello world", published_at: Time.current)
+# end
